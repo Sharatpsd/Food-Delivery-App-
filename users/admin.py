51b8b07545cd8e77b62d654f4831250d93fd62a8
Sharat Acharjee mugdho <Sharatacharjee6@gmail.com>
2026-02-27 00:08:17 +0600

    Enhance CustomUserAdmin with additional fields

--- users/admin.py
+++ users/admin.py
@@ -2,11 +2,24 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'name', 'role', 'phone', 'is_active', 'date_joined']
+    list_filter = ['role', 'is_active', 'date_joined']
+    search_fields = ['username', 'name', 'phone']
+    readonly_fields = ['date_joined', 'last_login']
+
     fieldsets = (
-        (None, {'fields': ('username','password')}),
-        ("Personal", {'fields': ('name','phone','address','role')}),
-        ("Permissions", {'fields': ('is_active','is_superuser','is_staff','groups','user_permissions')}),
-        ("Important", {'fields': ('last_login','date_joined')}),
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('name', 'phone', 'address', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'name', 'role', 'phone', 'address'),
+        }),
     )
