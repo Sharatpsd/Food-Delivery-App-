50b9d76177954df1d3d7c30e8f6bffe4f708b847
Sharat Acharjee mugdho <Sharatacharjee6@gmail.com>
2026-02-26 23:01:14 +0600

    Add role field to user model with choices

--- users/models.py
+++ users/models.py
@@ -8,6 +8,7 @@ class User(AbstractUser):
         ('delivery', 'Delivery Boy'),
         ('admin', 'Admin'),
     )
+
     name = models.CharField(max_length=255)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
     phone = models.CharField(max_length=15, blank=True)
