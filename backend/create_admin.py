ce7a047e7eac14bca61305a795fb01aa0766477c
Sharat Acharja <sharatacharjee6@gmail.com>
2026-03-08 16:46:05 +0600

    fix admin reset

---
+++ backend/create_admin.py
@@ -0,0 +1,16 @@
+import django
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser(
+        "admin",
+        "admin@gmail.com",
+        "admin123"
+    )
