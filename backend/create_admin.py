f57201f20d91de3df3bd355b77e273013f71d6a0
Sharat Acharja <sharatacharjee6@gmail.com>
2026-03-08 17:16:09 +0600

    fix django path for render admin script

--- backend/create_admin.py
+++ backend/create_admin.py
@@ -1,5 +1,8 @@
 import os
 import django
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
